e38b629720a7ed21f5d92e3d2a6ba9fbc7ccdb54
Sam <zenpignoy@163.com>
2017-11-21 19:59:33 +0800

    partnerApp需要phone字段

--- dlz.framework.ssme/src/main/java/com/dlz/framework/ssme/shiro/UserInfo.java
+++ dlz.framework.ssme/src/main/java/com/dlz/framework/ssme/shiro/UserInfo.java
@@ -12,64 +12,97 @@ public class UserInfo extends AuthUser {
 	private static final long serialVersionUID = 1L;
 
 	private Long pType;
+
 	private String logo;
+
 	private String pRelation;
-	private List<Long> groups=new ArrayList<Long>();
-	/**  
+
+	private String phone;
+
+	private List<Long> groups = new ArrayList<Long>();
+
+	/**
 	 * 获取groups
-	 * @return groups groups  
+	 * 
+	 * @return groups groups
 	 */
 	public List<Long> getGroups() {
 		return groups;
 	}
-	/** 
-	 * 设置groups 
-	 * @param groups groups 
+
+	/**
+	 * 设置groups
+	 * 
+	 * @param groups
+	 *          groups
 	 */
 	public void setGroups(List<Long> groups) {
 		this.groups = groups;
 	}
-	
-	/**  
+
+	/**
 	 * 获取pType
-	 * @return pType pType  
+	 * 
+	 * @return pType pType
 	 */
 	public Long getpType() {
 		return pType;
 	}
-	/** 
-	 * 设置pType 
-	 * @param pType pType 
+
+	/**
+	 * 设置pType
+	 * 
+	 * @param pType
+	 *          pType
 	 */
 	public void setpType(Long pType) {
 		this.pType = pType;
 	}
-	/**  
+
+	/**
 	 * 获取logo
-	 * @return logo logo  
+	 * 
+	 * @return logo logo
 	 */
 	public String getLogo() {
 		return logo;
 	}
-	/** 
-	 * 设置logo 
-	 * @param logo logo 
+
+	/**
+	 * 设置logo
+	 * 
+	 * @param logo
+	 *          logo
 	 */
 	public void setLogo(String logo) {
 		this.logo = logo;
 	}
-	/**  
+
+	/**
 	 * 获取pRelation
-	 * @return pRelation pRelation  
+	 * 
+	 * @return pRelation pRelation
 	 */
 	public String getpRelation() {
 		return pRelation;
 	}
-	/** 
-	 * 设置pRelation 
-	 * @param pRelation pRelation 
+
+	/**
+	 * 设置pRelation
+	 * 
+	 * @param pRelation
+	 *          pRelation
 	 */
 	public void setpRelation(String pRelation) {
 		this.pRelation = pRelation;
 	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
 }
