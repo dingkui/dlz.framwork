5f00a8b886f4cb3c8d975e73e831ba7563825c92
丁奎 <dingxingtong@163.com>
2018-01-08 11:46:10 +0800

    no commit message

--- dlz.framework/src/test/java/com/dlz/common/util/MessageFormatTest.java
+++ dlz.framework/src/test/java/com/dlz/common/util/MessageFormatTest.java
@@ -1,10 +1,9 @@
 package com.dlz.common.util;
 
 import java.text.MessageFormat;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import com.dlz.framework.util.JacksonUtil;
+import com.dlz.framework.util.StringUtils;
 
 public class MessageFormatTest {
 	private static Pattern msgPattern = Pattern.compile("\\{[\\w]*[^\\d]+[\\w]*\\}");
@@ -16,59 +15,15 @@ public class MessageFormatTest {
 		return MessageFormat.format(msg, paras);
 	}
 	
-	private static Pattern myMsgPattern = Pattern.compile("\\{([^\\}]*)\\}");
-	static String myformatMsg(Object message, Object... paras) {
-		String msg = getStr(message, "");
-		if (paras==null) {
-			paras=new Object[]{null};
-		}
-		Matcher mat = myMsgPattern.matcher(msg);
-		StringBuffer sb = new StringBuffer();
-		int end=0;
-		int i=0;
-		while(mat.find()){
-			String indexStr=mat.group(1).replaceAll("[^\\d]*", "");
-			int index=0;
-			if(!"".equals(indexStr)){
-				index=Integer.parseInt(indexStr);
-			}else{
-				index=i;
-			}
-			sb.append(msg,end, mat.start());
-			if(paras.length>index){
-				sb.append(getStr(paras[index], null));
-			}else{
-				sb.append(mat.group(0));
-			}
-			end=mat.end();
-			i++;
-		}
-		
-		return sb.append(msg,end,msg.length()).toString();
-	}
-	
-	private static String getStr(Object obj,String defualt){
-		if(obj==null){
-			return defualt;
-		}
-		if (obj instanceof CharSequence) {
-			return obj.toString();
-		}
-		return JacksonUtil.getJson(obj);
-	}
-	
-	
-	
-	
-
 	public static void main(String[] args) {
 //		System.out.println(formatMsg("12zz{}3",1,2,3,4));
 //		System.out.println(myformatMsg("a{}b{9，56}b{3，2}d{}{}{9}",1,2,3,4));
 		String a=null;
-		System.out.println(myformatMsg("a{}b{1}c{}d{}{}",a));
-		System.out.println(myformatMsg("a{}b{1}c{}d{}{}",a,1,2,3,4,5,6));
-		System.out.println(myformatMsg("a{}b{1}c{}d{}{}",a));
-		System.out.println(myformatMsg("a{}b{1}c{}d{}{}",a));
-		System.out.println(myformatMsg("abcd"));
+		System.out.println(StringUtils.formatMsg("a{}b{1}c{}d{}{}",a));
+		System.out.println(StringUtils.formatMsg("a{}b{1}c{}d{}{}",a,1,2,3,4,5,6));
+		System.out.println(StringUtils.formatMsg("a{}b{1}c{}d{}{}",a));
+		System.out.println(StringUtils.formatMsg("a{}b{1}c{}d{}{}",a));
+		System.out.println(StringUtils.formatMsg("a{}}b{1}c{}d{}{}",a));
+		System.out.println(StringUtils.formatMsg("abcd"));
 	}
 }
