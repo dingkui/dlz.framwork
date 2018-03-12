9ffc76204b816d1a6da905c862e6692815fb9db7
Sam <zenpignoy@163.com>
2018-03-12 11:07:13 +0800

    阿里云短信服务

--- dlz.framework.ssme/src/main/java/com/dlz/apps/notice/db/service/impl/SmsServiceImpl.java
+++ dlz.framework.ssme/src/main/java/com/dlz/apps/notice/db/service/impl/SmsServiceImpl.java
@@ -2,79 +2,79 @@ package com.dlz.apps.notice.db.service.impl;
 
 import java.util.Map;
 
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.aliyun.mns.client.CloudAccount;
-import com.aliyun.mns.client.CloudTopic;
-import com.aliyun.mns.client.MNSClient;
-import com.aliyun.mns.common.ServiceException;
-import com.aliyun.mns.model.BatchSmsAttributes;
-import com.aliyun.mns.model.MessageAttributes;
-import com.aliyun.mns.model.RawTopicMessage;
-import com.aliyun.mns.model.TopicMessage;
+import com.aliyuncs.DefaultAcsClient;
+import com.aliyuncs.IAcsClient;
+import com.aliyuncs.dysmsapi.model.v20170525.SendSmsRequest;
+import com.aliyuncs.dysmsapi.model.v20170525.SendSmsResponse;
+import com.aliyuncs.profile.DefaultProfile;
+import com.aliyuncs.profile.IClientProfile;
 import com.dlz.apps.notice.db.dao.SmsMapper;
 import com.dlz.apps.notice.db.model.Sms;
 import com.dlz.apps.notice.db.service.SmsService;
+import com.dlz.framework.logger.MyLogger;
 import com.dlz.framework.ssme.base.service.impl.BaseServiceImpl;
+import com.dlz.framework.util.JacksonUtil;
 
 @Service
-@Transactional(rollbackFor=Exception.class)
+@Transactional(rollbackFor = Exception.class)
 public class SmsServiceImpl extends BaseServiceImpl<Sms, Long> implements SmsService {
 
-    @Autowired
-    public void setMapper(SmsMapper mapper) {
-        this.mapper=mapper;
-    }
-    
+	@Autowired
+	public void setMapper(SmsMapper mapper) {
+		this.mapper = mapper;
+	}
 
-	/**
-	 * Step 1. 获取主题引用
-	 */
-	private static CloudAccount account = new CloudAccount("LTAI66tVGfsIZSzg", "gtnRiacO2Jmnc0I4z5I9g6Ij5mkCUB", "http://1018586260602151.mns.cn-hangzhou.aliyuncs.com/");
-	private static MNSClient client = account.getMNSClient();
-	private static CloudTopic topic = client.getTopicRef("sms.topic-cn-hangzhou");
+	private static MyLogger logger = MyLogger.getLogger(SmsServiceImpl.class);
+
+	// 产品名称:云通信短信API产品,开发者无需替换
+	private static final String product = "Dysmsapi";
+	// 产品域名,开发者无需替换
+	private static final String domain = "dysmsapi.aliyuncs.com";
+
+	// 此处需要替换成开发者自己的AK(在阿里云访问控制台寻找)
+	private static final String accessKeyId = "LTAI66tVGfsIZSzg";
+	private static final String accessKeySecret = "gtnRiacO2Jmnc0I4z5I9g6Ij5mkCUB";
 
 	@Override
 	public Boolean sendMsg(String templateCode, String phone, Map<String, String> params) {
-		/**
-		 * Step 2. 设置SMS消息体（必须）
-		 *
-		 * 注：目前暂时不支持消息内容为空，需要指定消息内容，不为空即可。
-		 */
-		RawTopicMessage msg = new RawTopicMessage();
-		msg.setMessageBody("sms-message");
-		/**
-		 * Step 3. 生成SMS消息属性
-		 */
-		MessageAttributes messageAttributes = new MessageAttributes();
-		BatchSmsAttributes batchSmsAttributes = new BatchSmsAttributes();
-		// 3.1 设置发送短信的签名（SMSSignName）
-		batchSmsAttributes.setFreeSignName("藻钙云");
-		// 3.2 设置发送短信使用的模板（SMSTempateCode）
-		batchSmsAttributes.setTemplateCode(templateCode);
-		// 3.3 设置发送短信所使用的模板中参数对应的值（在短信模板中定义的，没有可以不用设置）
-		BatchSmsAttributes.SmsReceiverParams smsReceiverParams = new BatchSmsAttributes.SmsReceiverParams();
-		for(String key : params.keySet()){
-			smsReceiverParams.setParam(key, params.get(key));
-		}
-		// 3.4 增加接收短信的号码
-		batchSmsAttributes.addSmsReceiver(phone, smsReceiverParams);
-		messageAttributes.setBatchSmsAttributes(batchSmsAttributes);
 		try {
-			/**
-			 * Step 4. 发布SMS消息
-			 */
-			TopicMessage ret = topic.publishMessage(msg, messageAttributes);
-			return StringUtils.isNotBlank(ret.getMessageId()) ? true : false;
-		} catch (ServiceException se) {
-			se.printStackTrace();
-		} catch (Exception e) {
-			e.printStackTrace();
+			// 可自助调整超时时间
+			System.setProperty("sun.net.client.defaultConnectTimeout", "10000");
+			System.setProperty("sun.net.client.defaultReadTimeout", "10000");
+			
+			// 初始化acsClient,暂不支持region化
+			IClientProfile profile = DefaultProfile.getProfile("cn-hangzhou", accessKeyId, accessKeySecret);
+			DefaultProfile.addEndpoint("cn-hangzhou", "cn-hangzhou", product, domain);
+			IAcsClient acsClient = new DefaultAcsClient(profile);
+
+			// 组装请求对象-具体描述见控制台-文档部分内容
+			SendSmsRequest request = new SendSmsRequest();
+			// 必填:待发送手机号
+			request.setPhoneNumbers(phone);
+			// 必填:短信签名-可在短信控制台中找到
+			request.setSignName("藻钙云");
+			// 必填:短信模板-可在短信控制台中找到
+			request.setTemplateCode(templateCode);
+			
+			// 可选:模板中的变量替换JSON串,如模板内容为"亲爱的${name},您的验证码为${code}"时,此处的值为
+			request.setTemplateParam(JacksonUtil.writeValueAsString(params));
+			// 选填-上行短信扩展码(无特殊需求用户请忽略此字段)
+			// request.setSmsUpExtendCode("90997");
+			
+			// 可选:outId为提供给业务方扩展字段,最终在短信回执消息中将此值带回给调用者
+			// request.setOutId("yourOutId");
+			// hint 此处可能会抛出异常，注意catch
+			SendSmsResponse sendSmsResponse = acsClient.getAcsResponse(request);
+			if (sendSmsResponse.getCode() != null && sendSmsResponse.getCode().equals("OK")) {
+				return true;
+			}
+		} catch (Exception ex) {
+			logger.error(ex.getMessage(), ex);
 		}
-		client.close();
 		return false;
 	}
 }
