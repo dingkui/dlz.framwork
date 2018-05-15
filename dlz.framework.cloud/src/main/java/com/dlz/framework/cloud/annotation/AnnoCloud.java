4c08be6d1e3e7c54bdd3e18f0f302338fc9aef08
丁奎 <dingxingtong@163.com>
2018-05-15 09:32:37 +0800

    实现http客户端实现接口注解

--- dlz.framework.cloud/src/main/java/com/dlz/framework/cloud/annotation/AnnoCloud.java
+++ dlz.framework.cloud/src/main/java/com/dlz/framework/cloud/annotation/AnnoCloud.java
@@ -3,7 +3,9 @@ import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;  
+import java.lang.annotation.Target;
+
+import com.dlz.framework.springframework.iproxy.anno.AnnoApi;  
 /** 
  * spring cloud访问接口注解
  * Author: dk 2017-06-16
@@ -12,6 +14,7 @@ import java.lang.annotation.Target;
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
+@AnnoApi("httpClient")
 public @interface AnnoCloud {
 	String value() default ""; 
 }
