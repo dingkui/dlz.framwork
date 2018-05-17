552386cd0b3181d68dbf4f5b0523f33f639efcd0
丁奎 <dingxingtong@163.com>
2018-05-17 11:19:50 +0800

    schedual完善

--- dlz.framework/src/main/java/com/dlz/framework/quartz/JobMethod.java
+++ dlz.framework/src/main/java/com/dlz/framework/quartz/JobMethod.java
@@ -16,6 +16,7 @@ import org.quartz.TriggerKey;
 import org.quartz.impl.StdSchedulerFactory;
 import org.springframework.stereotype.Component;
 
+import com.dlz.framework.exception.CodeException;
 import com.dlz.framework.logger.MyLogger;
 import com.dlz.framework.quartz.bean.ScheduleJob;
 import com.dlz.framework.quartz.bean.ScheduleJobCron;
@@ -28,109 +29,89 @@ import com.dlz.framework.quartz.bean.ScheduleJobSimple;
  */
 @Component
 public class JobMethod {
+	private static MyLogger logger = MyLogger.getLogger(JobMethod.class);
 	Scheduler scheduler;
-	
 	JobMethod(){
 		SchedulerFactory sf = new StdSchedulerFactory();  
         try {
             scheduler = sf.getScheduler();  
             scheduler.start();
         } catch (SchedulerException e) {  
-            e.printStackTrace();  
+        	logger.error(e.getMessage(), e); 
         }  
 	}
-	
-	private static MyLogger log = MyLogger.getLogger(JobMethod.class);
-
 	/**
 	 * 添加一个任务，如果任务已经存在则更新该任务
 	 *
-	 * @param scheduleJob
+	 * @param job
 	 * @throws SchedulerException
 	 */
-	public void addAndResetScheduleJobOld(ScheduleJob scheduleJob) {
-		TriggerKey triggerKey = TriggerKey.triggerKey(scheduleJob.getJobName(), scheduleJob.getJobGroup());
-		CronTrigger trigger;
-		try {
-			trigger = (CronTrigger) scheduler.getTrigger(triggerKey);
-			if (null == trigger) {
-				JobDetail jobDetail = JobBuilder.newJob(QuartzJobFactory.class).withIdentity(scheduleJob.getJobName(), scheduleJob.getJobGroup()).build();
-
-				jobDetail.getJobDataMap().put("scheduleJob", scheduleJob);
-
-				CronScheduleBuilder scheduleBuilder = CronScheduleBuilder.cronSchedule(((ScheduleJobCron)scheduleJob).getCronExpression());
-
-				trigger = TriggerBuilder.newTrigger().withIdentity(scheduleJob.getJobName(), scheduleJob.getJobGroup()).withSchedule(scheduleBuilder).build();
-				scheduler.scheduleJob(jobDetail, trigger);
-			} else {
-				/* Trigger已存在，那么更新相应的定时设置 */
-				CronScheduleBuilder scheduleBuilder = CronScheduleBuilder.cronSchedule(((ScheduleJobCron)scheduleJob).getCronExpression());
-
-				/* 按新的cronExpression表达式重新构建trigger */
-				trigger = trigger.getTriggerBuilder().withIdentity(triggerKey).withSchedule(scheduleBuilder).build();
-
-				/* 按新的trigger重新设置job执行 */
-				scheduler.rescheduleJob(triggerKey, trigger);
-			}
-		} catch (SchedulerException e) {
-			log.error("Task init failed.", e);
+	public void addAndResetScheduleJob(ScheduleJob job) {
+		if(job==null || job.getJobName()==null){
+			throw new CodeException("任务不能为空");
 		}
-	}
-	/**
-	 * 添加一个任务，如果任务已经存在则更新该任务
-	 *
-	 * @param scheduleJob
-	 * @throws SchedulerException
-	 */
-	public void addAndResetScheduleJob(ScheduleJob scheduleJob) {
-		TriggerKey triggerKey = TriggerKey.triggerKey(scheduleJob.getJobName(), scheduleJob.getJobGroup());
+		TriggerKey triggerKey = TriggerKey.triggerKey(job.getJobName(), job.getJobGroup());
 		Trigger trigger;
 		try {
 			System.out.println(scheduler.checkExists(triggerKey));
 			trigger = scheduler.getTrigger(triggerKey);
 			if (null == trigger) {
-				JobDetail jobDetail = JobBuilder.newJob(QuartzJobFactory.class).withIdentity(scheduleJob.getJobName(), scheduleJob.getJobGroup()).build();
-				jobDetail.getJobDataMap().put("scheduleJob", scheduleJob);
-				if(scheduleJob instanceof ScheduleJobSimple){
-					ScheduleJobSimple scheduleJobSimple=(ScheduleJobSimple)scheduleJob;
-					SimpleScheduleBuilder sinpleSchedual = SimpleScheduleBuilder.simpleSchedule().withRepeatCount(scheduleJobSimple.getRepeatTimes());
-					if(scheduleJobSimple.getIntervalseconds()!=null){
-						sinpleSchedual.withIntervalInSeconds(scheduleJobSimple.getIntervalseconds());
+				JobDetail jobDetail = JobBuilder.newJob(QuartzJobFactory.class).withIdentity(getJobKey(job)).build();
+				jobDetail.getJobDataMap().put("scheduleJob", job);
+				TriggerBuilder<Trigger> triggerBuilder = TriggerBuilder.newTrigger().withIdentity(triggerKey);
+				if(job instanceof ScheduleJobSimple){
+					ScheduleJobSimple jobSimple=(ScheduleJobSimple)job;
+					SimpleScheduleBuilder sinpleSchedual = SimpleScheduleBuilder.simpleSchedule().withRepeatCount(jobSimple.getRepeatTimes());
+					if(jobSimple.getIntervalseconds()!=null){
+						sinpleSchedual.withIntervalInSeconds(jobSimple.getIntervalseconds());
 					}
-					trigger = TriggerBuilder.newTrigger().withIdentity(triggerKey).startAt(scheduleJobSimple.getStartTime()).withSchedule(sinpleSchedual).build();
-				}else if(scheduleJob instanceof ScheduleJobCron){
-					ScheduleJobCron scheduleJobCron=(ScheduleJobCron)scheduleJob;
-					CronScheduleBuilder scheduleBuilder = CronScheduleBuilder.cronSchedule(scheduleJobCron.getCronExpression());
-					trigger = TriggerBuilder.newTrigger().withIdentity(triggerKey).withSchedule(scheduleBuilder).build();
+					if(jobSimple.getStartTime()!=null){
+						triggerBuilder.startAt(jobSimple.getStartTime());
+					}
+					trigger = triggerBuilder.withSchedule(sinpleSchedual).build();
+				}else if(job instanceof ScheduleJobCron){
+					ScheduleJobCron jobCron=(ScheduleJobCron)job;
+					String cronExpression = jobCron.getCronExpression();
+					if(cronExpression==null){
+						throw new CodeException("任务表达式不能为空！");
+					}
+					CronScheduleBuilder scheduleBuilder = CronScheduleBuilder.cronSchedule(cronExpression);
+					trigger = triggerBuilder.withSchedule(scheduleBuilder).build();
 				}
-
 				scheduler.scheduleJob(jobDetail, trigger);
 			} else {
 				/* Trigger已存在，那么更新相应的定时设置 */
-				if(scheduleJob instanceof ScheduleJobSimple){
-					ScheduleJobSimple scheduleJobSimple=(ScheduleJobSimple)scheduleJob;
+				if(job instanceof ScheduleJobSimple){
+					ScheduleJobSimple scheduleJobSimple=(ScheduleJobSimple)job;
 					SimpleScheduleBuilder simpleSchedual = SimpleScheduleBuilder.simpleSchedule().withRepeatCount(scheduleJobSimple.getRepeatTimes());
 					if(scheduleJobSimple.getIntervalseconds()!=null){
 						simpleSchedual.withIntervalInSeconds(scheduleJobSimple.getIntervalseconds());
 					}
 					/* 重新构建trigger */
-					trigger = ((SimpleTrigger)trigger).getTriggerBuilder().startAt(scheduleJobSimple.getStartTime()).withIdentity(triggerKey).withSchedule(simpleSchedual).build();
-				}else if(scheduleJob instanceof ScheduleJobCron){
-					ScheduleJobCron scheduleJobCron=(ScheduleJobCron)scheduleJob;
+					TriggerBuilder<SimpleTrigger> triggerBuilder = ((SimpleTrigger)trigger).getTriggerBuilder();
+					if(scheduleJobSimple.getStartTime()!=null){
+						triggerBuilder.startAt(scheduleJobSimple.getStartTime());
+					}
+					trigger = triggerBuilder.withSchedule(simpleSchedual).build();
+				}else if(job instanceof ScheduleJobCron){
+					ScheduleJobCron scheduleJobCron=(ScheduleJobCron)job;
 					CronScheduleBuilder scheduleBuilder = CronScheduleBuilder.cronSchedule(scheduleJobCron.getCronExpression());
-
 					/* 按新的cronExpression表达式重新构建trigger */
-					trigger = ((CronTrigger)trigger).getTriggerBuilder().withIdentity(triggerKey).withSchedule(scheduleBuilder).build();
+					trigger = ((CronTrigger)trigger).getTriggerBuilder().withSchedule(scheduleBuilder).build();
 				}
 				/* 按新的trigger重新设置job执行 */
 				scheduler.rescheduleJob(triggerKey, trigger);
 			}
 		} catch (SchedulerException e) {
-			log.error("Task init failed.", e);
+			logger.error(job);
+			logger.error("任务初始化失败", e);
 		}
 	}
 	
 	private JobKey getJobKey(ScheduleJob scheduleJob){
+		if(scheduleJob==null || scheduleJob.getJobName()==null){
+			throw new CodeException("任务不能为空");
+		}
 		return JobKey.jobKey(scheduleJob.getJobName(), scheduleJob.getJobGroup());
 	}
 
@@ -144,7 +125,7 @@ public class JobMethod {
 		try {
 			scheduler.pauseJob(getJobKey(scheduleJob));
 		} catch (SchedulerException e) {
-			log.error("Task pause failed.", e);
+			logger.error("Task pause failed.", e);
 		}
 	}
 
@@ -158,7 +139,8 @@ public class JobMethod {
 		try {
 			scheduler.resumeJob(getJobKey(scheduleJob));
 		} catch (SchedulerException e) {
-			log.error("Task resume failed.", e);
+			logger.error(scheduleJob);
+			logger.error("Task resume failed.", e);
 		}
 	}
 
@@ -172,7 +154,8 @@ public class JobMethod {
 		try {
 			scheduler.deleteJob(getJobKey(scheduleJob));
 		} catch (SchedulerException e) {
-			log.error("Task delete failed.", e);
+			logger.error(scheduleJob);
+			logger.error("Task delete failed.", e);
 		}
 	}
 
@@ -186,23 +169,21 @@ public class JobMethod {
 		try {
 			scheduler.triggerJob(getJobKey(scheduleJob));
 		} catch (SchedulerException e) {
-			log.error("Task run failed.", e);
+			logger.error(scheduleJob);
+			logger.error("Task run failed.", e);
 		}
 	}
 	
 	/**
 	 * 判断表达式是否可用 @param cron @return @throws
 	 */
-	public static boolean checkCron(String cron) {
+	public boolean checkCron(String cron) {
 		try {
 			CronScheduleBuilder.cronSchedule(cron);
 		} catch (Exception e) {
+			logger.equals(e.getMessage());
 			return (false);
 		}
 		return (true);
-	}
-	
-	public static void main(String[] args) {
-		System.out.println(checkCron("0/5 * * * * ? *"));
 	}
 }
