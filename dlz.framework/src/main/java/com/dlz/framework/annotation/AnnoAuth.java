ec1495900dcf7bdc3b0a842c5d16e373b81b0ee4
丁奎 <dingxingtong@163.com>
2018-05-23 14:35:39 +0800

    annoAuth支持继承

--- dlz.framework/src/main/java/com/dlz/framework/annotation/AnnoAuth.java
+++ dlz.framework/src/main/java/com/dlz/framework/annotation/AnnoAuth.java
@@ -1,5 +1,6 @@
 package com.dlz.framework.annotation;
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;  
@@ -9,7 +10,8 @@ import java.lang.annotation.Target;
  * Since: 
  */  
 @Retention(RetentionPolicy.RUNTIME)   
-@Target({ElementType.METHOD,ElementType.TYPE})   
+@Target({ElementType.METHOD,ElementType.TYPE})  
+@Inherited
 public @interface AnnoAuth  
 {  
 	String value() default "*"; 
