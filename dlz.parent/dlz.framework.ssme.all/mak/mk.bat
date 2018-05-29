fdd9750d02b8c9706e1cb521e9ee640986d5a1c3
丁奎 <dingxingtong@163.com>
2018-05-29 17:14:44 +0800

    添加dlz.parent及weball 工程

---
+++ dlz.parent/dlz.framework.ssme.all/mak/mk.bat
@@ -0,0 +1,20 @@
+@echo off
+cd /d %~dp0
+cd ..
+set m=src\main
+set r=..\..
+mkdir %m%
+
+xcopy %r%\dlz.framework\%m%\* %m% /e/y/Q
+xcopy %r%\dlz.web\%m%\* %m% /e/y/Q
+xcopy %r%\dlz.framework.ssme\%m%\* %m% /e/y/Q
+
+
+rd /s /q target
+@call mvn clean install -Dmaven.test.skip -offline
+rd /s /q %m%
+rd /s /q target\classes
+rd /s /q target\generated-sources
+rd /s /q target\maven-archiver
+rd /s /q target\maven-status
+pause
