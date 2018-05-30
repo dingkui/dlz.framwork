f0abf03f000a0979eb72b0054c0bb8610596b279
lxm <1055385715@qq.com>
2018-05-30 15:29:50 +0800

    1

--- dlz.framework/src/main/java/com/dlz/framework/util/system/Reflections.java
+++ dlz.framework/src/main/java/com/dlz/framework/util/system/Reflections.java
@@ -10,6 +10,7 @@ import java.lang.reflect.Type;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.aop.support.AopUtils;
 import org.springframework.util.Assert;
 
 import com.dlz.framework.annotation.NotDbField;
@@ -194,11 +195,7 @@ public class Reflections {
 	 */
 	public static Method getMethod(final Object obj, final String name,Class<?>... parameterTypes) {
 		String methodName = name.intern();
-		Class<?> searchType = obj.getClass();
-		if(searchType.getName().indexOf("BySpringCGLIB")>-1){
-			searchType=searchType.getSuperclass();
-		}
-		for (; searchType != Object.class; searchType = searchType.getSuperclass()) {
+		for (Class<?> searchType =AopUtils.getTargetClass(obj); searchType != Object.class; searchType = searchType.getSuperclass()) {
 			Method[] methods = searchType.getDeclaredMethods();
 			for (Method method : methods) {
 				if (method.getName()==methodName && arrayContentsEq(parameterTypes, method.getParameterTypes())) {
