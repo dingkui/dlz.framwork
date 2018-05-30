958a0998a12995245f8805b862846da7b5d5a056
lfeng.li <1003067988@qq.com>
2018-05-30 10:50:57 +0800

    erp

--- dlz.framework.ssme/src/main/java/com/dlz/framework/ssme/shiro/ShiroUrlFilter.java
+++ dlz.framework.ssme/src/main/java/com/dlz/framework/ssme/shiro/ShiroUrlFilter.java
@@ -4,17 +4,22 @@ import java.io.IOException;
 
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.shiro.subject.Subject;
 import org.apache.shiro.web.filter.authz.AuthorizationFilter;
 import org.springframework.http.HttpStatus;
 
+import com.dlz.web.holder.ThreadHolder;
+
 public class ShiroUrlFilter extends AuthorizationFilter {
 
     @Override
 	public boolean isAccessAllowed(ServletRequest request, ServletResponse response, Object mappedValue) throws IOException {
-        Subject subject = getSubject(request, response);
+    	ThreadHolder.setHttpRequest((HttpServletRequest)request);
+    	ThreadHolder.setHttpResponse((HttpServletResponse)response);
+    	Subject subject = getSubject(request, response);
 
         boolean isPermitted = true;
         String requestURI = getPathWithinApplication(request);
