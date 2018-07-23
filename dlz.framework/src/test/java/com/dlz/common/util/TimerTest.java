6dcf36afcc807656a63840be6c5ddfe7740e285a
丁奎 <dingxingtong@163.com>
2018-07-23 10:48:07 +0800

    httpUtil

---
+++ dlz.framework/src/test/java/com/dlz/common/util/TimerTest.java
@@ -0,0 +1,34 @@
+package com.dlz.common.util;
+
+
+import java.rmi.RemoteException;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import org.junit.Test;
+import org.springframework.util.StopWatch.TaskInfo;
+
+public class TimerTest {
+
+	Timer timer=new Timer();
+	/**
+	 * 检索多条数据
+	 * @throws Exception
+	 */
+	@Test
+	public void getList() throws Exception{
+		 TimerTask tasks=new TimerTask() {
+			 int i=0;
+				@Override
+				public void run() {
+							System.out.println(" 完成："+i+++"%");
+							if(i==10){
+								this.cancel();
+							}
+					
+				}
+			};
+			timer.schedule(tasks, 5000, 1000);
+			Thread.sleep(20000);
+	}
+}
