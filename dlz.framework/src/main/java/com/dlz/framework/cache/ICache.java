4fff34955073b19ff8af7c4366b751bc9fadfce9
丁奎 <59461202@qq.com>
2023-07-28 17:09:07 +0800

    ICache增加 put(String name, Serializable key, Serializable value); <T extends Serializable> T get(String name, Serializable key);

--- dlz.framework/src/main/java/com/dlz/framework/cache/ICache.java
+++ dlz.framework/src/main/java/com/dlz/framework/cache/ICache.java
@@ -6,6 +6,14 @@ import java.lang.reflect.Type;
 public interface ICache {
     <T extends Serializable> T get(String name, Serializable key, Type tClass);
 
+    default <T extends Serializable> T get(String name, Serializable key) {
+        return get(name, key, null);
+    }
+
+    default void put(String name, Serializable key, Serializable value) {
+        put(name, key, value, -1);
+    }
+
     void put(String name, Serializable key, Serializable value, int seconds);
 
     void remove(String name, Serializable key);
