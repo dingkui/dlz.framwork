c44e64375acb7159c8a8972c119d824ecba843de
丁奎 <59461202@qq.com>
2023-08-04 11:54:36 +0800

    测试SqlHelper自动生成字段

--- dlz.framework.db/dlz.framework.db.mybatis-plus/src/test/java/com/dlz/test/service/Dict.java
+++ dlz.framework.db/dlz.framework.db.mybatis-plus/src/test/java/com/dlz/test/service/Dict.java
@@ -1,13 +1,34 @@
 package com.dlz.test.service;
 
 import com.baomidou.mybatisplus.annotation.TableName;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
+import net.sf.jsqlparser.expression.DateTimeLiteralExpression;
+
+import java.math.BigDecimal;
+import java.util.Date;
 
 @Data
 @TableName("t_b_dict")
+@ApiModel("测试")
 public class Dict {
+    @ApiModelProperty("xasd：啊实打实\n\"xas\"")
     private String dictStatus;
-    private String dictName;
-    private String dictId;
-    private String dictType;
+    @ApiModelProperty("int")
+    private int a2;
+    @ApiModelProperty("boolean：")
+    private boolean a3;
+    @ApiModelProperty("Long：")
+    private Long a4;
+    @ApiModelProperty("Float：")
+    private Float a5;
+    @ApiModelProperty("BigDecimal：")
+    private BigDecimal a6;
+    @ApiModelProperty("Object：")
+    private Object a7;
+    @ApiModelProperty("Date：")
+    private Date a8;
+    @ApiModelProperty("java.sql.Date：")
+    private java.sql.Date a9;
 }
--- dlz.framework.db/dlz.framework.db.mybatis-plus/src/test/java/com/dlz/test/service/SimpleTest.java
+++ dlz.framework.db/dlz.framework.db.mybatis-plus/src/test/java/com/dlz/test/service/SimpleTest.java
@@ -1,15 +1,17 @@
 package com.dlz.test.service;
 
+import com.dlz.framework.db.helper.util.SqlHelper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@ContextConfiguration("")
 public class SimpleTest {
+    @Autowired
+    SqlHelper sqlHelper;
     @Test
     public void doTest() {
         int num = new Integer(1);
